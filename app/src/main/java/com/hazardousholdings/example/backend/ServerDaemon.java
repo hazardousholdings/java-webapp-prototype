6188dc643276ce04938b0597d703eb63efdd4a0e
Brian Henk <public@brianhenk.com>
2012-09-03 20:25:37 -0400

    Missed these two

--- app/src/main/java/com/hazardousholdings/example/backend/ServerDaemon.java
+++ app/src/main/java/com/hazardousholdings/example/backend/ServerDaemon.java
@@ -1,4 +1,23 @@
 package com.hazardousholdings.example.backend;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 public class ServerDaemon {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting Backend...");
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		webAppContext.setResourceBase("webapp");
+		webAppContext.setDescriptor("webapp/WEB-INF/web.xml");
+
+		Server server = new Server(8080);
+		server.setHandler(webAppContext);
+
+		server.start();
+		System.out.println("Started Backend.");
+		server.join();
+	}
+
 }
--- web/src/main/java/com/hazardousholdings/example/frontend/ServerDaemon.java
+++ web/src/main/java/com/hazardousholdings/example/frontend/ServerDaemon.java
@@ -1,4 +1,23 @@
 package com.hazardousholdings.example.frontend;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 public class ServerDaemon {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting Frontend...");
+		WebAppContext webAppContext = new WebAppContext();
+		webAppContext.setContextPath("/");
+		webAppContext.setResourceBase("webapp");
+		webAppContext.setDescriptor("webapp/WEB-INF/web.xml");
+
+		Server server = new Server(8080);
+		server.setHandler(webAppContext);
+
+		server.start();
+		System.out.println("Started Frontend.");
+		server.join();
+	}
+
 }
